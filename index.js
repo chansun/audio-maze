ccd9d31c328e4aebbb9a0a159d30b970e6a22ff0
Chansun Park <chansun@Macintosh.local>
2020-04-18 23:23:10 -0400

    test

--- index.js
+++ index.js
@@ -1,99 +1,4 @@
 
-// 라운드 별로 어떻게 넘어갈껀지, UI/UX어떻게 짤껀지 생각.
-
-// 데스크탑, 모바일에 따라 싸이즈 맞춰서 만들기.
-
-// grid에 따라 start, end 사이즈랑 위치 조절하기
-
-// Maze 랜덤으로 만들기. 
-
-// 라운드 별로 grid, board, maze 업데이트. 
-
-// css로 예쁘게 꾸미기, transition (라운드 div 만들고, Start! 창띄게하고, 3-2-1 카운트 센후 Round# 창 띄게하고, 시간지나면 자동으로 다음라운드로 넘어가게. 그 링크 닷 게임처럼 만들기)
-// https://state2-dot-to.com
-// https://www.youtube.com/watch?v=_s8PGt883os 게임 사운드 뽑기
-// https://www.leshylabs.com/apps/sfMaker/ (pickup 5 or pickup 이랑 loss 이렇게 두개써보자.)
-// https://www.youtube.com/watch?v=U5GUASL9OBs - success 1
-// https://www.youtube.com/watch?v=p7g7eBVGTLQ&list=LL6_eyp7Y6_toLHmS9E9F3YA&index=14&t=0s (아쉬운 노래)
-
-/*
-
-1. 모바일에서 터치만 인식하게 만들고.
-
-1.2 모바일에서 오디오 노래 안나옴. 예전에 러닝백 그거는 모바일에서 노래 나오던데. 오디오 트는법 찾아보기.
-
-2. 메이즈 조금더 많이, 간단하게만들기
-
-3. 게임 작동되는거 끗나면 파일 리팩토링, 지저분한거 다 없애고 깃헙에 깔끔하게 올리기
-
-4. 깃헙에 올리면서 설명, 사용방법, 사용된기술, 왜 만들게됫는지 이런거 정리해서 올리기
-
-5. 비스쿨, 깃헙 코드 정리, 레쥬메 정리, 김지현 그분 이메일 보내기 
-
-
-*/
-
-
-
-/*
-터치방법:
-https://stackoverflow.com/questions/13510999/when-to-use-touchmove-vs-mousemove
-https://stackoverflow.com/questions/43936084/how-to-make-mousemove-event-working-for-touchscreen-with-touchmove
-
-*/
-
-/*
-
-모바일까지 끗내고, 데스크탑 여러 브라우저에서 테스팅, 모바일에서 아이폰 갤럭시 테스팅. 코드/파일 깃헙에 정리해서 올리고 publish하기.
-
-b-school 일 끗내고, 비스쿨 코드 깃헙에 정리해서 올리기.
-
-레쥬메 정리/업데이트 하기 (job description, 프로젝트)
-
-다 햇으면 김지현 그 분한테 이메일보내서 이번학기 마무리하면서 레쥬메 업데이트를 했는데 follow up하고싶다고 연락.
-그리고 업데이트된 레쥬메, 이번학기에 작업한 그 오디오 메이즈(게임 퍼블리쉬 링크)랑 비스쿨에 사용된 프로그램 깃헙 링크 첨부하고,
-인터뷰때 말씀했던 평소에 코딩을 자주하는지, 그리고 코딩경험 얼마정도인지 물어본게 기억나서 이번학기동안 파트타임으로 일하면서 작업했던거 정리해서
-보여주고 싶었다고 말하기.
-
-이번뺄까 - 송명근 면접관이랑 인터뷰를 봤는데, 인터뷰질문중에 프로젝트나 관련 프로그래밍 경험을 물으셨는데,
-프로젝트 설명은 항상 문서로만 작업해오고 구술로 프로젝트에 설명해본 경험이많이 없어서 당시 너무 간추려서 이야기했는데, 이런게 좀 아쉽다고 느껴져서
-그동안 작업한거 정리하는도중 생각나서 팔로우업 하면서 보여주고 싶었다고 말하기?
-
-
-
-a) 다음단계로 넘어갈때 
-1. 레벨 넘어간다고 목소리, 
-2. 짝짝짝 박수 소리
-
-b) Start 누르면
-1. 게임 스타트 목소리
-2. 배경음? (ambience music 같은걸로, 그리고 tone.js 음이랑 좀 어울려야함)
-
-c) Retry 누르면
-1. 게임 스타트 목소리
-2. 배경음? (ambience music 같은걸로, 그리고 tone.js 음이랑 좀 어울려야함)
-
-d) Timer expried 됫을때
-1. 아쉬운 아~ 소리
-2. Timer expried 목소리 넣을까? 필요없을려나
-
-e) 마지막 단계 까지 깻을때
-1. intro2 그거 배경음으로 쓸까?
-2. All Levels Clear! Congrutulations! 목소리
-
-f) 시간 10초남앗을때
-1. 목소리로 10초남았다고 말하기.
-
-g) 시간 30초 남았을때
-1. 목소리로 30초남았다고 말하기.
-
-h) 터치로 구동하게 만들기. 
-1. 마우스 그거만 터치 드래깅으로 바꾸면 될것같은데
-2. 레이아웃 모바일용으로 작게만들기.
-
-
-
-*/
 
 
 function voice_make(sentence) {
@@ -132,7 +37,7 @@ let canvas, ctx, grid, board, maze, index, empty_color, filled_color, text_font,
 all_clear = false;
 
 function timer_begin() {
-    let s = 20;
+    let s = 60;
     let ms = 0;
     clearInterval(timer_loop);
     timer_loop = setInterval(function() {
@@ -140,14 +45,7 @@ function timer_begin() {
         if (ms == 0) {
             if (s <= 0) { // the timer expired, so move to the "retry" state
 
-                // version 1
-                /*
-                audio.src = playlist["outro"];
-                audio.volume = 0.7;
-                audio.play();
-                */
-
-                // version 2
+      
                 audio.play();
 
 
@@ -269,7 +167,7 @@ function getMousePos(ev) {
 
 function getTouchPos(ev) { 
     let rect = canvas.getBoundingClientRect();
-    return [ev.touches[0].clientY-rect.top+1, ev.touches[0].clientX-rect.left+1];
+    return [(ev.touches[0].clientY-rect.top+1)*5/3, (ev.touches[0].clientX-rect.left+1)*5/3];
 }
 
 
@@ -297,11 +195,8 @@ function tracker(coor, maze, index, canvas, grid, board) {
     if ((coor[0] >= maze[level][index][0]*(canvas.height/grid) && coor[0] < ((maze[level][index][0])+1)*(canvas.height/grid)) && (coor[1] >= maze[level][index][1]*(canvas.width/grid) && coor[1] < ((maze[level][index][1])+1)*(canvas.width/grid))) {
         board[maze[level][index][0]][maze[level][index][1]] = 1;
 
-        note_th_update();
-        synth.triggerAttackRelease(`${notes[index%7]}${note_th}`, "100n");
-        
-
-
+        // note_th_update();
+        // synth.triggerAttackRelease(`${notes[index%7]}${note_th}`, "100n");
 
         index = index + 1;
         if (maze[level].length == index) {
@@ -322,7 +217,8 @@ function tracker(coor, maze, index, canvas, grid, board) {
 
 
 function touch_motion(ev) {
-    //$("#root").on("mousemove touchstart", function(ev) {
+    let coor;
+    let index_temp;
         if (game_on) {
             coor = getTouchPos(ev); // coor = [y, x]
             //speechSynthesis.speak(voice_make("start"));
@@ -333,14 +229,6 @@ function touch_motion(ev) {
                     console.log("clear!!");
                     if (level == max_level) { // the current level is the last level, so move to the "retry" state
 
-                        // version 1
-                        /*
-                        audio.src = playlist["outro"];
-                        audio.volume = 0.7;
-                        audio.play();
-                        */
-
-                        // version 2
                         audio.play();
 
                         all_clear = true;
@@ -403,99 +291,11 @@ function touch_motion(ev) {
 
 // function mouseHandler(canvas, ctx, maze, index, board, grid, empty_color, filled_color) {
 function mouseHandler() {
-    let coor;
-    let index_temp;
-
+    
     canvas.addEventListener("touchstart", touch_motion);
     canvas.addEventListener("touchmove", touch_motion);
     canvas.addEventListener("touchend", touch_motion);
 
-    /*
-    canvas.addEventListener("touchstart", function(ev) {
-        ev.preventDefault();
-        let temp = getTouchPos(ev);
-        console.log(`temp: ${temp}`);
-        speechSynthesis.speak(voice_make("start"));
-    });
-    canvas.addEventListener("touchmove", function(ev) {
-        ev.preventDefault();
-        let temp = getTouchPos(ev);
-        console.log(`temp: ${temp}`);
-        speechSynthesis.speak(voice_make("move"));
-    });
-    */
-
-
-    /*
-    canvas.addEventListener("mousemove", function(ev) {
-        if (game_on) {
-            coor = getMousePos(ev); // coor = [y, x]
-            console.log(`coor: ${coor}`);
-            index_temp = tracker(coor, maze, index, canvas, grid, board);
-            if (index_temp > index) { // Correct Path
-                if (maze[level].length == index_temp) { // Cleared the current level;
-                    console.log("clear!!");
-                    if (level == max_level) { // the current level is the last level, so move to the "retry" state
-
-                        // version 1
-                        
-                        //audio.src = playlist["outro"];
-                        //audio.volume = 0.7;
-                        //audio.play();
-                        
-
-                        // version 2
-                        audio.play();
-
-                        all_clear = true;
-                        game_on = false;
-                        clearInterval(timer_loop);
-                        response = update_state("state1");
-                        response.then(function(result) {
-                            console.log(result);
-                            index = 0;
-                            level = 1
-                            $("#level").html(level);
-                            board = board_generate(grid);
-                            update(ctx, board, grid, empty_color, filled_color);
-                        });
-                    }
-                    else { // move to the next level
-                        game_on = false;
-                        clearInterval(timer_loop);
-                        response = update_state("state2");
-                        response.then(function(result){
-                            console.log(result);
-                            index = 0;
-                            level = level + 1
-                            $("#level").html(level);
-                            board = board_generate(grid);
-                            update(ctx, board, grid, empty_color, filled_color);
-                            game_on = true;
-                            timer_begin();
-                        })
-                    }
-                }
-                update(ctx, board, grid, empty_color, filled_color);
-                // 이 index는 tracker에서 index가 없데이트 되지 않기대문에 여기에 넣은거임.
-                index = index + 1; // index를 다음단계로 넘겨야함. 그래야 메이즈의 다음 path를 찾을수 있음.
-            }
-            // Stayed on the same position, nothing happened
-            else if (index_temp == index) {
-                update(ctx, board, grid, empty_color, filled_color);
-            }
-            // Wrong path, reset the grid
-            else {
-                // console.log("reset");
-                board = board_generate(grid);
-                update(ctx, board, grid, empty_color, filled_color);
-                index = 0;
-            }
-            tracker(coor, maze, index, canvas, grid, board);
-            update(ctx, board, grid, empty_color, filled_color); 
-        };
-    });
-    */
     
 };
 
@@ -573,126 +373,27 @@ async function update_state(keyword) {
 
 $(document).ready(function () {
 
-    // window.addEventListener("scroll", preventMotion, false);
-
-
-    // $("#state2").on("touchmove", function(ev) {
-    //     ev.preventDefault();
-        
-    //     console.log("=========");
-    //     console.log(ev);
-    //     console.log(ev.screenX);
-    //     console.log(ev.clientX);
-    //     console.log(ev.clientY);
-    // });
-
-    /*
-    document.getElementById("state2").addEventListener("touchmove", function(ev) {
-        console.log("MOOOVE");
-        console.log(ev.screenX);
-        console.log(ev.clientX);
-        console.log(ev.clientY);
-    });*/
-
-    // This one
-    /*
-    document.getElementById("state2").addEventListener("touchstart", function(e) {
-        e.preventDefault();
-        //let temp = getTouchPos(e);
-        //console.log(temp);
-        //console.log("===========");
-        //synth.triggerAttackRelease("C4", "10n");
-        speechSynthesis.speak(voice_make("Test"));
-    });
-
-
-    document.getElementById("state2").addEventListener("touchmove", function(e) {
-        e.preventDefault();
-        //let temp = getTouchPos(e);
-        //console.log(temp);
-        //console.log("===========");
-        //synth.triggerAttackRelease("C4", "10n");
-        speechSynthesis.speak(voice_make("Test"));    
-    });
-    */
-
-
-
-    /*
-    document.getElementById("state2").addEventListener("touchmove", function(ev) {
-    //$("body").on("touchmove", function(ev) {
-        // Cache the client X/Y coordinates
-
-        ev.preventDefault();
-
-        synth.triggerAttackRelease("C4", "10n");
-
-        console.log("===========");
-        let temp = getTouchPos(ev);
-        console.log(temp);
-        console.log("===========");
-        // clientX = e.touches[0].clientX;
-        // clientY = e.touches[0].clientY;
-        // console.log("===========");
-        // console.log(`clientY: ${clientY}`);
-        // console.log(`clientX: ${clientX}`);
-        // console.log("===========");
-
-    }, false);
-    */
-      
-
-    
-
     canvas_init();
     update(ctx, board, grid, empty_color, filled_color);
-    // mouseHandler(canvas, ctx, maze, index, board, grid, empty_color, filled_color);
     mouseHandler();
     $("#state1").on("click", function() {
-
-        // version 1
-        /*
         audio.src = playlist["outro"];
-        audio.volume = 0.0;
+        audio.volume = 0.7;
         audio.play();
-        */
-
-        // version 2
-       audio.src = playlist["outro"];
-       audio.volume = 0.7;
-       audio.play();
-       audio.pause();
-
-
-
-
-        //audio.volume = 0.0;
-        //audio.pause();
-
+        audio.pause();
         update(ctx, board, grid, empty_color, filled_color);
         response = update_state("init");
         response.then(function(result) {
-
             console.log(result);
             timer_begin();
             game_on = true;
         })
     });
 
-    
     $("#state1").on("click", async () => {
         await Tone.start();
         console.log('audio is ready');
-        //create a synth and connect it to the master output (your speakers)
         synth = new Tone.Synth().toMaster();
-        // synth.volume.value = -5;
-        //play a middle 'C' for the duration of an 8th note
-        // synth.triggerAttackRelease("C4", "10n");
-        // music start
-        
-
-
-
     });
 
 
