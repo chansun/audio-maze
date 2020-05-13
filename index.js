c086830195622162e482e8a31587c65104bc9047
Chansun Park <chansun@Macintosh.local>
2020-05-13 01:34:31 -0400

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class Model {
     constructor() {
         this.state = {
             page : "menu",
-            word : "starty", 
+            word : "start", 
             level : 1, 
             max_level : 15,
             index: 0,
